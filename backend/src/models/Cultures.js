45009bcc5e8945ae0b830aa59f4d86925d252035
Akash Singh <developer.akash8@gmail.com>
2023-07-29 19:07:54 +0530

    models: added for Cultures

---
+++ backend/src/models/Cultures.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ContentSchema = new mongoose.Schema({
+  imageLink: { type: String, required: true },
+  descriptions: { type: [String], required: true },
+});
+
+const CommentSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  comment: { type: String, required: true },
+});
+
+const CultureSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: [ContentSchema], required: true },
+  likes: { type: Number, default: 0 },
+  comments: { type: [CommentSchema] },
+});
+
+const Cultures = mongoose.model("cultures", CultureSchema);
+
+module.exports = Cultures;
