6de485a7096f76b350ddd8ee8c8373404dfd18ec
Akash Singh <developer.akash8@gmail.com>
2023-07-29 19:45:14 +0530

    server - connected to database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(process.env.MONGODB_URI);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
