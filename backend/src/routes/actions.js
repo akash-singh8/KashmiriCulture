efd98f5b1d139df77d58043e6f7e79a525eb03bd
Akash Singh <developer.akash8@gmail.com>
2023-07-30 01:31:29 +0530

    route: added for comment

--- backend/src/routes/actions.js
+++ backend/src/routes/actions.js
@@ -29,4 +29,35 @@ actionRouter.put("/like", authenticate, async (req, res) => {
   }
 });
 
+actionRouter.put("/comment", authenticate, async (req, res) => {
+  const { cultureID, username, comment } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(cultureID)) {
+    return res.status(400).send({ message: "Invalid cultureID" });
+  }
+
+  try {
+    const result = await Cultures.updateOne(
+      { _id: cultureID },
+      {
+        $push: {
+          comments: {
+            username: username,
+            comment: comment,
+          },
+        },
+      }
+    );
+
+    if (result.matchedCount > 0) {
+      res.sendStatus(200);
+    } else {
+      res.status(404).send({ message: "Culture not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
 module.exports = actionRouter;
