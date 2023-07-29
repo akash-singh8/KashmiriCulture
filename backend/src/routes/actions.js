a9d4d3781f1b677d7504aae25c2d88d0d62af60f
Akash Singh <developer.akash8@gmail.com>
2023-07-30 01:14:17 +0530

    route: added for like

---
+++ backend/src/routes/actions.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authenticate = require("../middlewares/auth.js");
+const Cultures = require("../models/Cultures.js");
+
+const actionRouter = express.Router();
+
+actionRouter.put("/like", authenticate, async (req, res) => {
+  const { cultureID } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(cultureID)) {
+    return res.status(400).send({ message: "Invalid cultureID" });
+  }
+
+  try {
+    const result = await Cultures.updateOne(
+      { _id: cultureID },
+      { $inc: { likes: 1 } }
+    );
+
+    if (result.matchedCount > 0) {
+      res.sendStatus(200);
+    } else {
+      res.status(404).send({ message: "Culture not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+module.exports = actionRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 const userRouter = require("./routes/users.js");
+const actionRouter = require("./routes/actions.js")
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/", userRouter);
+app.use("/", actionRouter);
 
 mongoose.connect(process.env.MONGODB_URI);
 
