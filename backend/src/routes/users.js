27db02f878f02cdc6f904238a22c6f497f153378
Akash Singh <developer.akash8@gmail.com>
2023-07-29 21:18:19 +0530

    routes: added for user signup

---
+++ backend/src/routes/users.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const Users = require("../models/Users.js");
+
+const userRouter = express.Router();
+
+userRouter.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // check if user already exists
+  const userExists = await Users.findOne({ email: email });
+
+  if (userExists) {
+    res.status(409).json({ message: "User already exists" });
+  } else {
+    const newUser = new Users({
+      name: name,
+      email: email,
+      password: password,
+    });
+    await newUser.save();
+    
+    const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, {
+      expiresIn: "1h",
+    });
+    res.json({ message: "User registered successfully", authToken: token });
+  }
+});
+
+module.exports = userRouter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const userRouter = require("./routes/users.js");
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use("/", userRouter);
 
 mongoose.connect(process.env.MONGODB_URI);
 
