f1895538d83989046a1795b5ef0c71354bfe6e85
Akash Singh <developer.akash8@gmail.com>
2023-07-29 22:42:56 +0530

    middleware: added for user authentication

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function authenticate(req, res, next) {
+  const token = req.headers.Authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+    const decodeToken = jwt.verify(token, process.env.SECRET_KEY);
+    req.userID = decodeToken.id;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token", error: err });
+  }
+}
+
+module.exports = authenticate;
