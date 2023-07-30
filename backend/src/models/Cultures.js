598f6a72b52ae196bfa1910280375b4c9a93a5ec
Akash Singh <developer.akash8@gmail.com>
2023-07-31 03:47:26 +0530

    model: updated Cultures

--- backend/src/models/Cultures.js
+++ backend/src/models/Cultures.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ContentSchema = new mongoose.Schema({
   imageLink: { type: String, required: true },
-  descriptions: { type: [String], required: true },
+  description: { type: String, required: true },
 });
 
 const CommentSchema = new mongoose.Schema({
