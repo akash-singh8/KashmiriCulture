e3196f6709a5673f03292d59db733e250ae21eb0
Akash Singh <developer.akash8@gmail.com>
2023-07-31 14:25:55 +0530

    cors: allow methods

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -2,6 +2,7 @@ function cors(req, res, next) {
 
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT");
 
   next();
 }
