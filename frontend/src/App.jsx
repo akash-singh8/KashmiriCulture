b301ea422eb5e7e5744e7f37ca427928df338a5e
Akash Singh <developer.akash8@gmail.com>
2023-07-31 10:30:19 +0530

    pages: added Signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Home from "./pages/home";
+import Signup from "./pages/Signup";
+// import Home from "./pages/home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+      <Signup />
       <Footer />
     </div>
   );
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,48 @@
+import "../css/register.css";
+
+function Signup() {
+  function handleSignup(e) {
+    e.preventDefault();
+
+    const input = e.target;
+    const name = input[0];
+    const email = input[1];
+    const password = input[2];
+    const confirm = input[3];
+
+    if (password.value != confirm.value){
+        alert("Password didnt' match!\nPlease enter same password")
+        return;
+    }
+
+    console.log("Name :", name.value);
+    console.log("Email :", email.value);
+    console.log("Password :", password.value);
+
+    name.value = "";
+    email.value = "";
+    password.value = "";
+    confirm.value = "";
+  }
+
+  return (
+    <section className="register">
+      <h1>Signup</h1>
+      <form className="form" onSubmit={handleSignup}>
+        <input type="text" placeholder="Enter your name" required />
+        <input type="email" placeholder="Enter your email" required />
+        <input type="password" placeholder="Enter password" required />
+        <input type="password" placeholder="Confirm password" required />
+        <button>Signup</button>
+      </form>
+
+      <div className="switch">
+        <p>
+          Already have an account? <span>Login</span>
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Signup;
